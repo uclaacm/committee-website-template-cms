2724324d01dff77783d4c1537e9816cb777f5ceb
Charles Zhu <churro@Charless-MacBook-Air.local>
2023-01-29 15:27:43 -0800

    lint changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,21 @@ const Home: NextPage = () => {
         <div className={styles.masthead}>
           <div className={styles['masthead-text']}>
             <div className={styles.heading}>
-              <h1 className={styles.title}>ACM&nbsp;
+              <h1 className={styles.title}>
+                ACM&nbsp;
                 <span className={styles['committee-name']}>Teach LA</span>
               </h1>
               <h2 className={styles.lead}>making coding accessible</h2>
             </div>
-            <p className={styles.description}>ACM Teach LA pairs UCLA
-            students with schools in Los Angeles to provide free
-            computer science classes. Our goal is to empower all
-            students with the ability to code, and use it to make a
-            difference.</p>
-            <a className={styles['cta-btn']} href='#'>Join Us</a>
+            <p className={styles.description}>
+              ACM Teach LA pairs UCLA students with schools in Los Angeles to
+              provide free computer science classes. Our goal is to empower all
+              students with the ability to code, and use it to make a
+              difference.
+            </p>
+            <a className={styles['cta-btn']} href="#">
+              Join Us
+            </a>
           </div>
         </div>
         <div className={styles['main-section']}>
