08b0168f9c5dd84162a86ba784279138c5546e27
elizabethkim11 <105129075+elizabethkim11@users.noreply.github.com>
2023-02-06 21:01:12 -0800

    Teampage (#11)
    * Add teampage.scss
    
    * set up temp grid
    
    * fixed for linting

--- pages/otherPage.tsx
+++ pages/otherPage.tsx
@@ -1,18 +1,43 @@
-import Link from 'next/link';
 import React from 'react';
 import MainLayout from '../components/MainLayout';
-import styles from '../styles/Home.module.scss';
+import styles from '../styles/Teampage.module.scss';
 
 export default function OtherPage() {
   return (
     <MainLayout>
       <div className={styles.main}>
-        <h1 className={styles.description}>
-          Here&apos;s another page you can navigate to using the navbar!
-        </h1>
-        <Link href="/">
-          <button>Go back home</button>
-        </Link>
+        <h1 className={styles.title}>Team</h1>
+
+        <div>
+          <h2 className={styles.subtitle}>Board</h2>
+          <p className={styles.description}>
+            Our directors, school leads, and curriculum leads. Feel free to
+            reach out to any of our board for more information about what we do!
+          </p>
+          <div className={styles.boardgrid}>
+            {/* 2 per row */}
+            <div className={styles.card}>temp</div>
+            <div className={styles.card}>temp</div>
+            <div className={styles.card}>temp</div>
+            <div className={styles.card}>temp</div>
+          </div>
+        </div>
+
+        <div>
+          <h2 className={styles.subtitle}>General Members</h2>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <div className={styles.gmgrid}>
+            {/* 3 per row */}
+            <div className={styles.card}>temp</div>
+            <div className={styles.card}>temp</div>
+            <div className={styles.card}>temp</div>
+          </div>
+        </div>
       </div>
     </MainLayout>
   );
