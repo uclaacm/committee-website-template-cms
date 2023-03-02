5c2f00720f6790efc26e249cda93ef0d53827f0c
elizabeth <elizabethkim03@gmail.com>
2023-03-01 17:35:30 -0800

    Update footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,11 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import styles from '../styles/Home.module.scss';
-import Image from 'next/image';
 import WordmarkLogo from '../public/acm-logo-wordmark-extended.png';
-import instagram from '../public/instagram.png';
 import discord from '../public/discord.png';
 import email from '../public/email.png';
+import instagram from '../public/instagram.png';
+import styles from '../styles/Home.module.scss';
 
 export default function Footer() {
   return (
@@ -14,28 +14,33 @@ export default function Footer() {
         <section className={styles.footerlogo}>
           <Link href="/">
             <a>
-              <Image src={WordmarkLogo} width={180} height={70} alt="Logo"/>
+              <Image src={WordmarkLogo} width={180} height={70} alt="Logo" />
             </a>
           </Link>
         </section>
         <section className={styles.footerlogo}>
           <Link href="https://www.instagram.com/acm.ucla">
             <a>
-              <Image src={instagram} width={30} height={30} alt="Instagram Icon"/>
+              <Image
+                src={instagram}
+                width={30}
+                height={30}
+                alt="Instagram Icon"
+              />
             </a>
           </Link>
         </section>
         <section className={styles.footerlogo}>
           <Link href="https://discord.com/invite/eWmzKsY">
             <a>
-              <Image src={discord} width={35} height={30} alt="Discord Icon"/>
+              <Image src={discord} width={35} height={30} alt="Discord Icon" />
             </a>
           </Link>
         </section>
         <section className={styles.footerlogo}>
           <Link href="mailto: acm@ucla.edu">
             <a>
-              <Image src={email} width={35} height={30} alt="Email Icon"/>
+              <Image src={email} width={35} height={30} alt="Email Icon" />
             </a>
           </Link>
         </section>
