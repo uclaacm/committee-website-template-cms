f15455faa0a98f4cedd1168056b0d376f37cbc1c
“ChiweiChen” <chiwei0705@gmail.com>
2023-03-11 22:50:49 -0800

    unresponsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,13 +21,25 @@ export default function Navbar() {
       </div>
       <div className="navbar-items">
         <div className="navbar-link">
-          <Link href="/">team</Link>
+          <Link href="/"><a className ={styles.navlink}>about</a></Link>
         </div>
         <div className="navbar-link">
-          <Link href="/eventsPage">events</Link>
+          <Link href="/"><a className ={styles.navlink}>blog</a></Link>
         </div>
         <div className="navbar-link">
-            <Link href="/" ><a className={styles.join}>Join Us</a></Link>
+          <Link href="/"><a className ={styles.navlink}>dev</a></Link>
+        </div>
+        <div className="navbar-link">
+          <Link href="/"><a className ={styles.navlink}>team</a></Link>
+        </div>
+        <div className="navbar-link">
+          <Link href="/"><a className ={styles.navlink}>curriculum</a></Link>
+        </div>
+        <div className="navbar-link">
+          <Link href="/eventsPage"><a className ={styles.navlink}>events</a></Link>
+        </div>
+        <div className="navbar-link" >
+            <Link href="/"><a id={styles.joinlink} >Join Us</a></Link>
         </div>
       </div>
     </nav>
