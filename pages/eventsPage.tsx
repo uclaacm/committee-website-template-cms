7cc453d775e0d295dfda6e4838eebb6cbbc35466
elizabeth <elizabethkim03@gmail.com>
2023-04-27 18:17:36 -0700

    add getstaticprops to events page

--- pages/eventsPage.tsx
+++ pages/eventsPage.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainLayout from '../components/MainLayout';
 import styles from '../styles/Events.module.scss';
+import getAllEvents from '../scripts/event-generator-sheets.mjs';
 
-export default function Test() {
+// import moment from 'moment';
+
+// const localizer = momentLocalizer(moment);
+
+// // see eventPropGetter
+// const getEventClassByEvent = (event) => {
+// 	let modifierStr = '';
+// 	if (event.committee) {
+// 		modifierStr = `rbc-override-${event.committee}`;
+// 	}
+// 	return ({
+// 		className: `rbc-override-event ${modifierStr}`,
+// 	});
+// };
+
+export default function Events({events}: {events: Array<Array>}) {
+  const [activeEvent, setActiveEvent] = useState(null);
+	const [indexedEvents, setIndexedEvents] = useState(events.map(({event, index}) => ({...event, id: index})));
   return (
     <MainLayout>
       <div className={styles.main}>
@@ -27,4 +45,20 @@ export default function Test() {
       </div>
     </MainLayout>
   );
-}
+};
+
+export const getStaticProps = async () => {
+	const events = await getAllEvents();
+	// Attempt to replace new lines with <br/>, doesnt work
+	// const processedEvents = events.map((event) => (
+	// 	{...event, description: <>{event.description.replace(/\n/g, '<br/>')}</>}));
+	// console.log(processedEvents);
+
+	return {
+		props: {
+			events: events,
+		},
+
+		revalidate: 3600,
+	};
+};
